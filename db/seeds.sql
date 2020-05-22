4503d0871b935bf8932b6d60af85bc496a552f28
AstridSuhartono <astrid_sh@yahoo.com>
2020-05-22 20:03:45 +0930

    handlebars section start

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 1', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 2', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 3', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 4', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 5', false);
+
